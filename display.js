b1ac8842518ff5a9f93dfae0d2606e1e4a0085a1
David Gibb <dgibb802@gmail.com>
2016-12-09 11:02:22 -0800

    Update display.js

--- display.js
+++ display.js
@@ -9,16 +9,6 @@ scrollY:0, 	//MEMORY[FF42]
 scrollX:0,	//MEMORY[FF43]
 line:0,		//MEMORY[FF44]
 
-bgOn:0,				//MEMORY[FF40] bit 0
-spritesOn:0,		//MEMORY[FF40] bit 1
-spriteSize:0,		//MEMORY[FF40] bit 2
-bgTileMap:0,		//MEMORY[FF40] bit 3
-tileSet:0,			//MEMORY[FF40] bit 4
-					//MEMORY[FF40] bit 0
-windowTileMap:0,    //MEMORY[FF40] bit 6
-lcdOn:0,			//MEMORY[FF40] bit 7
-
-
 step: function(){
 	
 	switch(display.mode){
@@ -37,7 +27,6 @@ step: function(){
 		case 1:
 			if(display.tCount>=456){
 				display.tCount=0;
-				display.mode=2;
 				display.line++;
 				if (display.line>153){
 					display.mode=2;
@@ -58,6 +47,7 @@ step: function(){
 				display.tCount=0;
 				display.mode=0;
 				display.drawScanline();
+				display.line++;
 				
 			}
 				break;
@@ -67,30 +57,34 @@ step: function(){
 showState: function(){
 console.log('gpu mode: ', display.mode);
 console.log('tCount: ', display.tCount);
-console.log('scrollY: ', display.scrollY);
-console.log('scrollX: ', display.scrollX);
-console.log('line: ', display.line);
+console.log('LCD Control (FF40): ', MEMORY[0xFF40].toString(16));
+console.log('scrollY: (FF42)', display.scrollY);
+console.log('scrollX: (FF43)', display.scrollX);
+console.log('line: (FF43)', display.line.toString(16));
 },
 
 drawScanline: function(){
-	dataOffset=display.line*640
-	var tileOffset =((display.scrollY+display.line)%8)*2
-	for(var i=0;i<20;i++){
-		var tile = (MEMORY[0xFF40]&0x10===0)?tileMap0[(display.scrollX/8+i)%32][((display.scrollY+display.line)/8)%32]:tileMap1[(display.scrollX/8+i)%32][((display.scrollY+display.line)/8)%32];
-		if (MEMORY[0xFF40]&0x80===0){tile=cpu.signDecode(tile)+128;}
-		tile+=tile;
-		var bit0=(MEMORY[0xFF40]&0x80===0)?tileSet0[tile+tileOffset]:tileSet1[tile+tileOffset];
-		var bit1=(MEMORY[0xFF40]&0x80===0)?tileSet0[tile+tileOffset+1]:tileSet1[tile+tileOffset+1];
-		for(var j=0;j<8;j++){
-			var pix=(bit1>>(8-j-1)&0x01)?2:0;
-			pix+=(bit0>>(8-j-1)&0x01)?1:0;
-			pixData.data[dataOffset]=paletteRef[pix];
-			pixData.data[dataOffset+1]=paletteRef[pix];
-			pixData.data[dataOffset+2]=paletteRef[pix];
-			dataOffset+=4;
-
+	
+	var dataOffset=display.line*640
+	var offsetY =((display.scrollY+display.line)%8)*2
+	
+	for(var i=0;i<160;i++){
+		
+		var offsetX=(scrollX+i)%8
+		var tileY=(Math.floor((display.scrollY+display.line)/8))%32;
+		var tileX=(Math.floor((scrollX+i)/8))%32
+		var tile = ((MEMORY[0xFF40]&0x08)===0x80)?tileMap1[tileY][tileX]:tileMap0[tileY][tileX];
+		if (MEMORY[0xFF40]&0x10===0){tile=cpu.signDecode(tile)+128;}
+		tile=16*tile;
+		var bit0=((MEMORY[0xFF40]&0x10)===0x10)?tileSet1[tile+offsetY]:tileSet0[tile+offsetY];
+		var bit1=((MEMORY[0xFF40]&0x10)===0x10)?tileSet1[tile+offsetY+1]:tileSet0[tile+offsetY+1];
+		var pix=(bit1>>(7-offsetX)&0x01)?2:0;
+		pix+=(bit0>>(7-offsetX)&0x01)?1:0;
+		pixData.data[dataOffset]=paletteRef[pix];
+		pixData.data[dataOffset+1]=paletteRef[pix];
+		pixData.data[dataOffset+2]=paletteRef[pix];
+		dataOffset+=4;
 		}
-	}
 },
 
 canvasInit: function(){
