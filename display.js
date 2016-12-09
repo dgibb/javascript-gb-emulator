d294c3559a7a2bffed136deb287d0898c68c647d
David Gibb <dgibb802@gmail.com>
2016-12-09 11:03:00 -0800

    Update display.js

--- display.js
+++ display.js
@@ -96,64 +96,12 @@ canvasInit: function(){
 	}
 	screen.putImageData(pixData,0,0);
 },
- 
-
-/*
-drawFrame: function(){
-	if(MEMORY[0xFF40]&0x01){
-	var tilemap=(MEMORY[0xFF40]&0x02===0x02)?0x9800 : 0x9C00;
-	var Start=tilemap+MEMORY[0xFF42]+MEMORY[0xFF43];
-	console.log('scrollX: ', MEMORY[0xFF43], 'scrollY: ', MEMORY[0xFF42] );
-	console.log('tilemap: ', tilemap.toString(16));
-	console.log('Start: ', Start.toString(16));
-	console.log(MEMORY[Start], MEMORY[Start+1], MEMORY[Start+2], MEMORY[Start+3], MEMORY[Start+4], MEMORY[Start+5], MEMORY[Start+6], MEMORY[Start+7], MEMORY[Start+8], MEMORY[Start+9], MEMORY[Start+10], MEMORY[Start+11], MEMORY[Start+12], MEMORY[Start+13], MEMORY[Start+14], MEMORY[Start+15], MEMORY[Start+16], MEMORY[Start+17], MEMORY[Start+18]);
-	Start+=32;
-	console.log(MEMORY[Start], MEMORY[Start+1], MEMORY[Start+2], MEMORY[Start+3], MEMORY[Start+4], MEMORY[Start+5], MEMORY[Start+6], MEMORY[Start+7], MEMORY[Start+8], MEMORY[Start+9], MEMORY[Start+10], MEMORY[Start+11], MEMORY[Start+12], MEMORY[Start+13], MEMORY[Start+14], MEMORY[Start+15], MEMORY[Start+16], MEMORY[Start+17], MEMORY[Start+18]);
-	Start+=32;
-	console.log(MEMORY[Start], MEMORY[Start+1], MEMORY[Start+2], MEMORY[Start+3], MEMORY[Start+4], MEMORY[Start+5], MEMORY[Start+6], MEMORY[Start+7], MEMORY[Start+8], MEMORY[Start+9], MEMORY[Start+10], MEMORY[Start+11], MEMORY[Start+12], MEMORY[Start+13], MEMORY[Start+14], MEMORY[Start+15], MEMORY[Start+16], MEMORY[Start+17], MEMORY[Start+18]);
-	Start+=32;
-	console.log(MEMORY[Start], MEMORY[Start+1], MEMORY[Start+2], MEMORY[Start+3], MEMORY[Start+4], MEMORY[Start+5], MEMORY[Start+6], MEMORY[Start+7], MEMORY[Start+8], MEMORY[Start+9], MEMORY[Start+10], MEMORY[Start+11], MEMORY[Start+12], MEMORY[Start+13], MEMORY[Start+14], MEMORY[Start+15], MEMORY[Start+16], MEMORY[Start+17], MEMORY[Start+18]);
-	Start+=32;
-	console.log(MEMORY[Start], MEMORY[Start+1], MEMORY[Start+2], MEMORY[Start+3], MEMORY[Start+4], MEMORY[Start+5], MEMORY[Start+6], MEMORY[Start+7], MEMORY[Start+8], MEMORY[Start+9], MEMORY[Start+10], MEMORY[Start+11], MEMORY[Start+12], MEMORY[Start+13], MEMORY[Start+14], MEMORY[Start+15], MEMORY[Start+16], MEMORY[Start+17], MEMORY[Start+18]);
-	Start+=32;
-	console.log(MEMORY[Start], MEMORY[Start+1], MEMORY[Start+2], MEMORY[Start+3], MEMORY[Start+4], MEMORY[Start+5], MEMORY[Start+6], MEMORY[Start+7], MEMORY[Start+8], MEMORY[Start+9], MEMORY[Start+10], MEMORY[Start+11], MEMORY[Start+12], MEMORY[Start+13], MEMORY[Start+14], MEMORY[Start+15], MEMORY[Start+16], MEMORY[Start+17], MEMORY[Start+18]);
-	Start+=32;
-	console.log(MEMORY[Start], MEMORY[Start+1], MEMORY[Start+2], MEMORY[Start+3], MEMORY[Start+4], MEMORY[Start+5], MEMORY[Start+6], MEMORY[Start+7], MEMORY[Start+8], MEMORY[Start+9], MEMORY[Start+10], MEMORY[Start+11], MEMORY[Start+12], MEMORY[Start+13], MEMORY[Start+14], MEMORY[Start+15], MEMORY[Start+16], MEMORY[Start+17], MEMORY[Start+18]);
-	Start+=32;
-	console.log(MEMORY[Start], MEMORY[Start+1], MEMORY[Start+2], MEMORY[Start+3], MEMORY[Start+4], MEMORY[Start+5], MEMORY[Start+6], MEMORY[Start+7], MEMORY[Start+8], MEMORY[Start+9], MEMORY[Start+10], MEMORY[Start+11], MEMORY[Start+12], MEMORY[Start+13], MEMORY[Start+14], MEMORY[Start+15], MEMORY[Start+16], MEMORY[Start+17], MEMORY[Start+18]);
-	Start+=32;
-	console.log(MEMORY[Start], MEMORY[Start+1], MEMORY[Start+2], MEMORY[Start+3], MEMORY[Start+4], MEMORY[Start+5], MEMORY[Start+6], MEMORY[Start+7], MEMORY[Start+8], MEMORY[Start+9], MEMORY[Start+10], MEMORY[Start+11], MEMORY[Start+12], MEMORY[Start+13], MEMORY[Start+14], MEMORY[Start+15], MEMORY[Start+16], MEMORY[Start+17], MEMORY[Start+18]);
-	Start+=32;
-	console.log(MEMORY[Start], MEMORY[Start+1], MEMORY[Start+2], MEMORY[Start+3], MEMORY[Start+4], MEMORY[Start+5], MEMORY[Start+6], MEMORY[Start+7], MEMORY[Start+8], MEMORY[Start+9], MEMORY[Start+10], MEMORY[Start+11], MEMORY[Start+12], MEMORY[Start+13], MEMORY[Start+14], MEMORY[Start+15], MEMORY[Start+16], MEMORY[Start+17], MEMORY[Start+18]);
-	Start+=32;
-	console.log(MEMORY[Start], MEMORY[Start+1], MEMORY[Start+2], MEMORY[Start+3], MEMORY[Start+4], MEMORY[Start+5], MEMORY[Start+6], MEMORY[Start+7], MEMORY[Start+8], MEMORY[Start+9], MEMORY[Start+10], MEMORY[Start+11], MEMORY[Start+12], MEMORY[Start+13], MEMORY[Start+14], MEMORY[Start+15], MEMORY[Start+16], MEMORY[Start+17], MEMORY[Start+18]);
-	Start+=32;
-	console.log(MEMORY[Start], MEMORY[Start+1], MEMORY[Start+2], MEMORY[Start+3], MEMORY[Start+4], MEMORY[Start+5], MEMORY[Start+6], MEMORY[Start+7], MEMORY[Start+8], MEMORY[Start+9], MEMORY[Start+10], MEMORY[Start+11], MEMORY[Start+12], MEMORY[Start+13], MEMORY[Start+14], MEMORY[Start+15], MEMORY[Start+16], MEMORY[Start+17], MEMORY[Start+18]);
-	Start+=32;
-	console.log(MEMORY[Start], MEMORY[Start+1], MEMORY[Start+2], MEMORY[Start+3], MEMORY[Start+4], MEMORY[Start+5], MEMORY[Start+6], MEMORY[Start+7], MEMORY[Start+8], MEMORY[Start+9], MEMORY[Start+10], MEMORY[Start+11], MEMORY[Start+12], MEMORY[Start+13], MEMORY[Start+14], MEMORY[Start+15], MEMORY[Start+16], MEMORY[Start+17], MEMORY[Start+18]);
-	Start+=32;
-	console.log(MEMORY[Start], MEMORY[Start+1], MEMORY[Start+2], MEMORY[Start+3], MEMORY[Start+4], MEMORY[Start+5], MEMORY[Start+6], MEMORY[Start+7], MEMORY[Start+8], MEMORY[Start+9], MEMORY[Start+10], MEMORY[Start+11], MEMORY[Start+12], MEMORY[Start+13], MEMORY[Start+14], MEMORY[Start+15], MEMORY[Start+16], MEMORY[Start+17], MEMORY[Start+18]);
-	Start+=32;
-	console.log(MEMORY[Start], MEMORY[Start+1], MEMORY[Start+2], MEMORY[Start+3], MEMORY[Start+4], MEMORY[Start+5], MEMORY[Start+6], MEMORY[Start+7], MEMORY[Start+8], MEMORY[Start+9], MEMORY[Start+10], MEMORY[Start+11], MEMORY[Start+12], MEMORY[Start+13], MEMORY[Start+14], MEMORY[Start+15], MEMORY[Start+16], MEMORY[Start+17], MEMORY[Start+18]);
-	Start+=32;
-	console.log(MEMORY[Start], MEMORY[Start+1], MEMORY[Start+2], MEMORY[Start+3], MEMORY[Start+4], MEMORY[Start+5], MEMORY[Start+6], MEMORY[Start+7], MEMORY[Start+8], MEMORY[Start+9], MEMORY[Start+10], MEMORY[Start+11], MEMORY[Start+12], MEMORY[Start+13], MEMORY[Start+14], MEMORY[Start+15], MEMORY[Start+16], MEMORY[Start+17], MEMORY[Start+18]);
-	Start+=32;
-	console.log(MEMORY[Start], MEMORY[Start+1], MEMORY[Start+2], MEMORY[Start+3], MEMORY[Start+4], MEMORY[Start+5], MEMORY[Start+6], MEMORY[Start+7], MEMORY[Start+8], MEMORY[Start+9], MEMORY[Start+10], MEMORY[Start+11], MEMORY[Start+12], MEMORY[Start+13], MEMORY[Start+14], MEMORY[Start+15], MEMORY[Start+16], MEMORY[Start+17], MEMORY[Start+18]);
-	Start+=32;
-	console.log(MEMORY[Start], MEMORY[Start+1], MEMORY[Start+2], MEMORY[Start+3], MEMORY[Start+4], MEMORY[Start+5], MEMORY[Start+6], MEMORY[Start+7], MEMORY[Start+8], MEMORY[Start+9], MEMORY[Start+10], MEMORY[Start+11], MEMORY[Start+12], MEMORY[Start+13], MEMORY[Start+14], MEMORY[Start+15], MEMORY[Start+16], MEMORY[Start+17], MEMORY[Start+18]);
-	Start+=32;
-	console.log(MEMORY[Start], MEMORY[Start+1], MEMORY[Start+2], MEMORY[Start+3], MEMORY[Start+4], MEMORY[Start+5], MEMORY[Start+6], MEMORY[Start+7], MEMORY[Start+8], MEMORY[Start+9], MEMORY[Start+10], MEMORY[Start+11], MEMORY[Start+12], MEMORY[Start+13], MEMORY[Start+14], MEMORY[Start+15], MEMORY[Start+16], MEMORY[Start+17], MEMORY[Start+18]);
-	Start+=32;
-	console.log(MEMORY[Start], MEMORY[Start+1], MEMORY[Start+2], MEMORY[Start+3], MEMORY[Start+4], MEMORY[Start+5], MEMORY[Start+6], MEMORY[Start+7], MEMORY[Start+8], MEMORY[Start+9], MEMORY[Start+10], MEMORY[Start+11], MEMORY[Start+12], MEMORY[Start+13], MEMORY[Start+14], MEMORY[Start+15], MEMORY[Start+16], MEMORY[Start+17], MEMORY[Start+18]);
-	}
-},
-*/
 
 pixDataInit: function(){
 	for(var i = 0; i<pixData.data.length;i++){
 		pixData.data[i]=255;
 	}
-}
+},
 
 		
 };
